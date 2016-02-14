95382c358534a5ae5038bb2ec1f6c329a994de67
Arthelon <hsing.daniel@gmail.com>
2016-02-14 14:59:52 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyCast3',
+    version='0.0.1',
+    packages=['PyCast3'],
+    url='https://github.com/Arthelon/PyCast3',
+    license='MIT License',
+    author='Daniel',
+    author_email='hsing.daniel@gmail.com',
+    description='Search for and download your favourite podcasts'
+)
