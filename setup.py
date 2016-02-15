2bdc2144a79978269f948610dffffa026a793d94
Arthelon <hsing.daniel@gmail.com>
2016-02-15 14:44:53 +0800

    Updated dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ setup(
     name='PyCast3',
     version='0.0.1',
     packages=['PyCast3'],
+    scripts=['bin/PyCast3'],
     url='https://github.com/Arthelon/PyCast3',
-    license='MIT License',
+    license='LICENSE.txt',
     author='Daniel',
     author_email='hsing.daniel@gmail.com',
-    description='Search for and download your favourite podcasts'
+    description='Search for and download your favourite podcasts',
+    install_requires=[
+        'requests',
+        'feedparser',
+        'clint',
+        'pytest'
+    ]
 )
